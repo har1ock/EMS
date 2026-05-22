bc2dd7d9b14e43c0b0c45582c2724ef27bad3d9d
harlock <xarlokua@gmail.com>
2026-05-22 23:14:28 +0300

    day5

--- app/deps.py
+++ app/deps.py
@@ -43,3 +43,16 @@ def get_current_user(
         raise credential_exception
     
     return user
+
+
+def require_admin(current_user: User = Depends(get_current_user)) -> User:
+    """
+    Залежність для перевірки, чи має користувач права адміністратора.
+    Якщо роль не 'admin' — повертає помилку 403 Forbidden.
+    """
+    if current_user.role != "admin": # type: ignore
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="У вас немає прав для виконання цієї дії. Потрібна роль 'admin'.",
+        )
+    return current_user
